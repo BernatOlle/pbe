38689349039a8e3d24e99f3ed75d6f5a8003e6dc
BernatOlle <bernatollef@gmail.com>
2022-10-03 16:26:06 +0100

    library

--- puzzle1.py
+++ puzzle1.py
@@ -1,18 +1,18 @@
 from RPLCD.i2c import CharLCD
 import sys
-#implementació de les llibreries
 
-def main():
-	lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
-				cols=20, rows=4, dotsize=8,
-				charmap='A02',
-				auto_linebreaks=True,
-				backlight_enabled=True) #inicialització de la LCD
+class LCD: 
+    def main(self):
+        lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
+                    cols=20, rows=4, dotsize=8,
+                    charmap='A02',
+                    auto_linebreaks=True,
+                    backlight_enabled=True) 
+        lcd.write_string("Escriu un text de un\n\rmaxim de 20x4 i prem\n\renter i Ctrl+D per \n\racabar ")
 
-	lcd.write_string("Escriu un text de un\n\rmaxim de 20x4 i prem\n\ renter i Ctrl+D per\n\racabar ")
-
-	frase = sys.stdin.readlines() #llegim el string multilinia
-	lcd.write_multiline(frase)#Utilitzem la funció creada per fer el string multilinia 
+        frase = sys.stdin.readlines()
+        lcd.write_multiline(frase)
 
 if __name__ == '__main__':
-      main()
+    l=LCD()
+    l.main()
