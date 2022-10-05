e13ddb348d07d10cd65cac97caa127e49f7bcc8d
BernatOlle <90847284+BernatOlle@users.noreply.github.com>
2022-10-05 08:49:11 +0200

    Update puzzle1.py

--- puzzle1.py
+++ puzzle1.py
@@ -3,11 +3,7 @@ import sys
 
 class LCD: 
     def main(self):
-        lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
-                    cols=20, rows=4, dotsize=8,
-                    charmap='A02',
-                    auto_linebreaks=True,
-                    backlight_enabled=True) 
+        lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1, cols=20, rows=4, dotsize=8,charmap='A02', auto_linebreaks=True, backlight_enabled=True) 
         lcd.write_string("Escriu un text de un\n\rmaxim de 20x4 i prem\n\renter i Ctrl+D per \n\racabar ")
 
         frase = sys.stdin.readlines()
