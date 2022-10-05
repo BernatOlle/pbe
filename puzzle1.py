71aada9dc55f332dccdb1637f472f763fce769f3
BernatOlle <90847284+BernatOlle@users.noreply.github.com>
2022-10-05 08:54:15 +0200

    Update puzzle1.py

--- puzzle1.py
+++ puzzle1.py
@@ -2,7 +2,7 @@ from RPLCD.i2c import CharLCD
 import sys
 
 class LCD: 
-    def main(self):
+    def lcd_multiline(self):
         lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1, cols=20, rows=4, dotsize=8,charmap='A02', auto_linebreaks=True, backlight_enabled=True) 
         lcd.write_string("Escriu un text de un\n\rmaxim de 20x4 i prem\n\renter i Ctrl+D per \n\racabar ")
 
@@ -11,4 +11,4 @@ class LCD:
 
 if __name__ == '__main__':
     l=LCD()
-    l.main()
+    l.lcd_multiline()
