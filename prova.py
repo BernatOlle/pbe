938f5d5f9e24d4bbb74419d2f715dff46e490f3f
BernatOlle <bernatollef@gmail.com>
2022-10-16 21:05:39 +0200

    prova

--- prova.py
+++ prova.py
@@ -17,21 +17,25 @@ class TextViewWindow(Gtk.Window):
         self.add(self.box)
 
         self.create_textview()
-        self.create_button()
+        self.create_toolbar()
 
-    def create_button(self):
-  
+    def create_toolbar(self):
+        toolbar = Gtk.Toolbar()
+        self.box.pack_start(toolbar, False,False,0)
+
+     
 
         button_clear = Gtk.ToolButton(label="Mostrar en LCD")
         button_clear.connect("clicked", self.on_print_clicked)
-        self.box.pack_start(button_clear,True,True,0)
+        toolbar.insert(button_clear,9)
 
     def create_textview(self):
- 
+        scrolledwindow = Gtk.ScrolledWindow()
+        self.box.pack_start(scrolledwindow, True,True,0)
 
         self.textview = Gtk.TextView()
         self.textbuffer = self.textview.get_buffer()
-        self.add(self.textview)
+        scrolledwindow.add(self.textview)
 
     def on_print_clicked(self, widget):
         l=LCD()
